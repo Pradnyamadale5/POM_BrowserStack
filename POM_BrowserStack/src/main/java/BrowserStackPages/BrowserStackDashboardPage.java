184e97376794550a1e92c8b580223725719d4b5f
Pradnyamadale5 <pradnya.madle.sndt@gmail.com>
2023-01-12 22:32:19 +0530

    New Browser Stack Project

---
+++ POM_BrowserStack/src/main/java/BrowserStackPages/BrowserStackDashboardPage.java
@@ -0,0 +1,36 @@
+package BrowserStackPages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import dev.failsafe.internal.util.Assert;
+
+public class BrowserStackDashboardPage
+{
+	//Object Repository
+	WebDriver driver;
+	By UserProfileBtn = By.xpath("//button[@id='account-menu-toggle']");
+	By Summary = By.xpath("//a[text()='Summary']");
+	By UserFullName = By.xpath("(//span[@class='profile-details-card__list-item-value-text'])[1]");
+	
+	//Constructor
+	public BrowserStackDashboardPage(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+	
+	//Methods
+	public void UserProfileClick()
+	{
+		driver.findElement(UserProfileBtn).click();
+	}
+	public void ClickonSummary()
+	{
+		driver.findElement(Summary).click();
+	}
+	public String verifyUserName()
+	{
+		 String getUserText = driver.findElement(UserFullName).getText();
+		return getUserText;
+	}
+}
